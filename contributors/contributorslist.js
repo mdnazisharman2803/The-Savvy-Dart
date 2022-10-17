d0a93a8b1b57b8ef3f28f494262dbffe56257936
Aksha mishra <84321203+aksha123-git@users.noreply.github.com>
2022-10-17 23:58:24 +0530

    Add contributors list

---
+++ contributors/contributorslist.js
@@ -0,0 +1,33 @@
+contributors = [
+    {
+      id: 1,
+      fullname: "Mujore Umme Faatimah-Iz-Zaahra",
+      username: "https://github.com/fz3hra",
+    },
+    {
+      id: 2,
+      fullname: "Md Nazish Arman",
+      username: "https://github.com/mdnazisharman2803",
+    },
+    {
+      id: 3,
+      fullname: "Mradula Mittal",
+      username: "https://github.com/Mradula-Mittal",
+    },
+    {
+      id: 4,
+      fullname: "Waly",
+      username: "https://github.com/waly226",
+    },
+    {
+      id: 5,
+      fullname: "Alexis YAO",
+      username: "https://github.com/misteral1009",
+    },
+    {
+      id: 6,
+      fullname: "Aksha Mishra",
+      username: "https://github.com/aksha123-git",
+    },
+  ];
+  
